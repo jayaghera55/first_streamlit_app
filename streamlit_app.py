3bc9125c6fd0bd6c089628c04b41a9f56be879c6
jayaghera55 <89829110+jayaghera55@users.noreply.github.com>
2023-05-15 14:49:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.title('''Breakfast Menu
     Hard-Boiled Free-Range Egg''')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text'🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text(' Omega 3 & Blueberry Oatmeal')
+streamlit.text'Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
