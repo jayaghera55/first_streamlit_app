1399c9b23f746c11eb1c29fea196ddc8943cfaec
jayaghera55 <89829110+jayaghera55@users.noreply.github.com>
2023-05-15 14:42:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit
 
 streamlit.title('My Paraent Helathy Diner')
 
-streamlit.title('My Paraent Helathy Diner')
+streamlit.title('''Breakfast Menu
+                Omega 3 & Bluebarry Oatmeal
+                Kale Spinach & Rocket Smoothie
+                Hard-Boiled Free-Range Egg''')
