666a0eee74caca60d0f4eab6120390b12443373c
jayaghera55 <89829110+jayaghera55@users.noreply.github.com>
2023-05-15 14:46:46 +0530

    Update streamlit_app.py
    added breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,8 @@ streamlit.title('''Breakfast Menu
     Omega 3 & Bluebarry Oatmeal
     Kale Spinach & Rocket Smoothie
     Hard-Boiled Free-Range Egg''')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
